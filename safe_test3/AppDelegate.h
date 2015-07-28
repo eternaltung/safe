d5b137b95fd4e3b80cc596a1551f13d6d56e0d05
=bbiiggppiigg <=bbiiggppiigg@gmail.com>
2015-07-28 15:06:48 +0800

    Initial Commit

---
+++ safe_test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  safe_test3
+//
+//  Created by bbiiggppiigg on 2015/7/28.
+//  Copyright (c) 2015年 bbiiggppiigg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ safe_test3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  safe_test3
+//
+//  Created by bbiiggppiigg on 2015/7/28.
+//  Copyright (c) 2015年 bbiiggppiigg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
